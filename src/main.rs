f12098f65203303ae6b06cfce0b877d4f2c97893
Marek Onuszko <marek.onuszko@gmail.com>
2017-12-23 20:40:22 +0100

    error message for bad argument

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,18 @@ extern crate rusty_pixels;
 use std::env;
 use std::process;
 
+use rusty_pixels::argparams::Config;
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = rusty_pixels::argparams::Config::new(&args).unwrap();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
     
     if let Err(e) = rusty_pixels::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
